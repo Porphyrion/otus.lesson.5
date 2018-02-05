44149826a460aff6a239c92f62b1af083c403871
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-05 07:24:49 +0000

    Deploy Porphyrion/otus.lesson.5 to github.com/Porphyrion/otus.lesson.5.git:gh-pages

--- search/all_b.js
+++ search/all_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['_7eapp',['~App',['../class_app.html#a34f1f253b1cef5f4ecbac66eaf6964ec',1,'App']]],
   ['_7eprimitive',['~Primitive',['../class_primitive.html#a05288f40c90569a902e845a75b314fb3',1,'Primitive']]]
 ];
